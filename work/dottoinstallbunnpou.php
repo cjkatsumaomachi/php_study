bc9857c337565e5e8be2c664b27ebce08f253cd9
yusuke.watanabe <yusuke-watanabe@cosmo-japan.net>
2021-08-19 18:53:28 +0900

    8/19

--- work/dottoinstallbunnpou.php
+++ work/dottoinstallbunnpou.php
@@ -139,3 +139,136 @@ function sum($a, $b, $c)
 echo sum(100, 300, 500) . PHP_EOL; // 900
 echo sum(-1000, 300, 500) . PHP_EOL; // 0
 
+
+// '#27  nullも渡せるようにしよう'
+
+<?php
+
+declare(strict_types=1);
+
+function getAward(int $score): ?string
+{
+  return $score >= 100 ? 'Gold Medal' : 'まいこちゃん';
+}
+
+echo getAward(150) . PHP_EOL;
+echo getAward(29) . PHP_EOL;
+
+
+
+// '#28 条件演算子を使ってみよう'
+<?php
+
+// $score1 = 90;
+// $score2 = 40;
+// $score3 = 100;
+
+$scores = [
+  90,
+  40,
+  100,
+];
+
+
+echo $scores[1] . PHP_EOL;
+$scores[1] = 60;
+echo $scores[1] . PHP_EOL;
+
+
+
+// '#30 条件演算子を使ってみよう'
+<?php
+
+$scores = [
+  'first'  => 90, 
+  'second' => 40, 
+  'third'  => 100,
+];
+
+// var_dump($scores);
+// print_r($scores);
+
+echo $scores['third'] . PHP_EOL;
+
+//#31 foreachで配列を操作しよう
+
+<?php
+
+$scores = [
+  'first'  => 90, 
+  'second' => 40, 
+  'third'  => 100,
+];
+
+// foreach ($scores as $value) {
+// foreach ($scores as $score) {
+//   echo $score . PHP_EOL;
+// }
+
+foreach ($scores as $key => $score) {
+  echo $key . ' - ' . $score . PHP_EOL;
+}
+
+//#32 配列の要素を展開しよう
+
+<?php
+
+$moreScores = [
+  55,
+  72,
+  'perfect',
+  [90, 42, 88],
+];
+
+$scores = [
+  90,
+  40,
+  ...$moreScores,
+  100,
+];
+
+// print_r($scores);    
+
+echo $scores[5][2] . PHP_EOL;
+
+
+//#33 可変長引数を表現しよう
+
+
+<?php
+
+// function sum($a, $b, $c)
+
+function sum(...$numbers)
+{
+  // return $a + $b + $c;
+  $total = 0;
+  foreach ($numbers as $number) {
+    $total += $number;
+  }
+  return $total;
+}
+
+echo sum(1, 3, 5) . PHP_EOL;
+echo sum(4, 2, 5, 1) . PHP_EOL;
+
+//#34 複数の返り値を受け取ってみよう
+
+<?php
+
+function getStats(...$numbers)
+{
+  $total = 0;
+  foreach ($numbers as $number) {
+    $total += $number;
+  }
+  return [$total, $total / count($numbers)];
+}
+
+// print_r(getStats(1, 3, 5));
+
+// list($sum, $average) = getStats(1, 3, 5);
+[$sum, $average] = getStats(1, 3, 5);
+
+echo $sum . PHP_EOL;
+echo $average . PHP_EOL;
